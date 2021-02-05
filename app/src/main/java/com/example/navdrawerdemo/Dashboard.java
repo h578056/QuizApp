6bae36861f20293f10ddc56e0dabc1163ebb4a99
hvl579680 <vegy_93@hotmail.com>
2021-02-05 13:38:11 +0100

    added rooms database

--- app/src/main/java/com/example/navdrawerdemo/Dashboard.java
+++ app/src/main/java/com/example/navdrawerdemo/Dashboard.java
@@ -3,6 +3,7 @@ package com.example.navdrawerdemo;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.room.Room;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -19,11 +20,14 @@ import android.widget.TextView;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
     public static final int GET_FROM_GALLERY = 3;
     DrawerLayout drawerLayout;
     ImageView newCat;
+    public List<CatObject> catList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,33 +35,46 @@ public class Dashboard extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
 
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").allowMainThreadQueries().build();
+        CatDAO catDAO = db.catDAO();
+
+        catList = new ArrayList<>();
+        catList = catDAO.getAll();
+
     }
-    public void clickMenu(View view){
+
+    public void clickMenu(View view) {
         //open drawer
         MainActivity.openDrawer(drawerLayout);
     }
-    public void clickLogo(View view){
+
+    public void clickLogo(View view) {
         //close drawer
         MainActivity.closeDrawer(drawerLayout);
     }
-    public void clickHome(View view){
+
+    public void clickHome(View view) {
         //redirect home
         MainActivity.redirectActivity(this, MainActivity.class);
     }
-    public void clickDashBoard(View view){
+
+    public void clickDashBoard(View view) {
         //recreate activity
         recreate();
     }
-    public void clickAboutUs(View view){
+
+    public void clickAboutUs(View view) {
         //redirect to about us
         MainActivity.redirectActivity(this, AboutUs.class);
     }
-    public void clickLogout(View view){
+
+    public void clickLogout(View view) {
         //close app
         MainActivity.logout(this);
     }
+
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         //close drawer
         MainActivity.closeDrawer(drawerLayout);
@@ -85,24 +102,29 @@ public class Dashboard extends AppCompatActivity {
             }
         }
     }
-    public void addCat (View View) {
+
+    public void addCat(View View) {
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").allowMainThreadQueries().build();
+        CatDAO catDAO = db.catDAO();
 
         //finding new name
         EditText mEdit = (EditText) findViewById(R.id.catNameInput);
         String newCatName = mEdit.getText().toString();
-        TextView catName= (TextView) findViewById(R.id.catNameOfAddedCat);
+        TextView catName = (TextView) findViewById(R.id.catNameOfAddedCat);
         catName.setText(newCatName);
         newCat.getDrawable();
-        ImageView imgV= (ImageView) findViewById(R.id.CatImage);
+        ImageView imgV = (ImageView) findViewById(R.id.CatImage);
         imgV.setImageDrawable(newCat.getDrawable());
-        CatObject upladedCat= new CatObject(newCatName, R.id.newCatImage);
 
+        CatObject uploadedCat = new CatObject(catList.size() + 1, newCatName, R.id.newCatImage);
+        catList.add(uploadedCat);
+        catDAO.insertAll(uploadedCat);
         //test
-        System.out.println(newCatName +newCat.getDrawable() );
+        System.out.println(newCatName + newCat.getDrawable());
 
 
     }
-
 
 
 }
--- app/src/main/java/com/example/navdrawerdemo/MainActivity.java
+++ app/src/main/java/com/example/navdrawerdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.navdrawerdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.room.Room;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -16,15 +17,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     public DrawerLayout drawerLayout;
-    public ArrayList<CatObject> catList;
-    public String scoreTxt="Score";
+    public List<CatObject> catList;
+    public String scoreTxt = "Score";
     public CatObject currentCat;
-    public int correctAns=0;
-    public int totalAns=0;
+    public int correctAns = 0;
+    public int totalAns = 0;
 
 
     @Override
@@ -32,59 +34,85 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawerLayout = findViewById(R.id.drawer_layout);
-        catList= new ArrayList<>();
-        catList.add(new CatObject("bengal cat", R.drawable.bengal_icon));
+        catList = new ArrayList<>();
+/*        catList.add(new CatObject("bengal cat", R.drawable.bengal_icon));
         catList.add(new CatObject("persian cat", R.drawable.persian_icon));
-        catList.add(new CatObject("siameser cat",R.drawable.siameser_cat_cart));
+        catList.add(new CatObject("siameser cat", R.drawable.siameser_cat_cart));*/
+
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "database-name").allowMainThreadQueries().build();
+        CatDAO catDAO = db.catDAO();
+        catList = catDAO.getAll();
+
+        if (catList.size() == 0) {
+            catList.add(new CatObject(0, "bengal cat", R.drawable.bengal_icon));
+            catList.add(new CatObject(1, "persian cat", R.drawable.persian_icon));
+            catList.add(new CatObject(2, "siameser cat", R.drawable.siameser_cat_cart));
+            catDAO.insertAll(catList.get(0));
+            catDAO.insertAll(catList.get(1));
+            catDAO.insertAll(catList.get(2));
+            catList = catDAO.getAll();
+        } else {
+            catList = catDAO.getAll();
+        }
+
         getRandomImage();
     }
-    private void getRandomImage(){
-        ImageView mImageView = (ImageView)findViewById(R.id.myImageView);
-        CatObject catRandObj= getRandomElement(catList);
-        currentCat= catRandObj;
-        int catObjectImageCode= catRandObj.getImageName(); //catList.get(imageId).getImageName()
+
+    private void getRandomImage() {
+        ImageView mImageView = (ImageView) findViewById(R.id.myImageView);
+        CatObject catRandObj = getRandomElement(catList);
+        currentCat = catRandObj;
+        int catObjectImageCode = catRandObj.getImageName(); //catList.get(imageId).getImageName()
         mImageView.setBackgroundResource(catObjectImageCode);
     }
-    public CatObject getRandomElement(ArrayList<CatObject> list)
-    {
+
+    public CatObject getRandomElement(List<CatObject> list) {
         Random rand = new Random();
         return list.get(rand.nextInt(list.size()));
     }
 
-    public void clickMenu(View view){
+    public void clickMenu(View view) {
         openDrawer(drawerLayout);
     }
-    public static void openDrawer(DrawerLayout drawerLayout){
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
         drawerLayout.openDrawer((GravityCompat.START));
     }
-    public void clickLogo(View view){
+
+    public void clickLogo(View view) {
         //close drawer
         closeDrawer(drawerLayout);
     }
-    public static void closeDrawer(DrawerLayout drawerLayout){
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
         //close drawer layout
-        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             //close drawer if open
             drawerLayout.closeDrawer(GravityCompat.START);
         }
     }
-    public void clickHome(View view){
+
+    public void clickHome(View view) {
         //recreate activity
         recreate();
     }
-    public void clickDashBoard(View view){
+
+    public void clickDashBoard(View view) {
         //redirect to dashboard
         redirectActivity(this, Dashboard.class);
     }
-    public void clickAboutUs(View view){
+
+    public void clickAboutUs(View view) {
         redirectActivity(this, AboutUs.class);
     }
-    public void clickLogout(View view){
+
+    public void clickLogout(View view) {
         logout(this);
     }
-    public static void logout(Activity activity){
+
+    public static void logout(Activity activity) {
         //alert dialog
-        AlertDialog.Builder builder= new AlertDialog.Builder(activity);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle("Exit");
         builder.setMessage("Do you want to exit app?");
         builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
@@ -105,13 +133,15 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.show();
     }
+
     public static void redirectActivity(Activity activity, Class aclass) {
         Intent intent = new Intent(activity, aclass);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         activity.startActivity(intent);
     }
+
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         //close drawer
         closeDrawer(drawerLayout);
@@ -119,17 +149,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void answerQuestion(View view) {
         //if correct or not
-        final EditText editText1= findViewById(R.id.editText1);
+        final EditText editText1 = findViewById(R.id.editText1);
         String myString = editText1.getText().toString();
-        if(myString!=null){
-            myString= myString.toLowerCase();
+        if (myString != null) {
+            myString = myString.toLowerCase();
         }
 
-        totalAns=totalAns+1;
-        if(myString.equals(currentCat.getCatName())){
-            correctAns=correctAns+1;
+        totalAns = totalAns + 1;
+        if (myString.equals(currentCat.getCatName())) {
+            correctAns = correctAns + 1;
             increaseScore();
-        }else{
+        } else {
             showAns();
         }
         increaseScore();
@@ -139,11 +169,12 @@ public class MainActivity extends AppCompatActivity {
     public void increaseScore() {
         TextView displayInteger = (TextView) findViewById(
                 R.id.score);
-        displayInteger.setText("Score: "+ correctAns+"/"+totalAns );
+        displayInteger.setText("Score: " + correctAns + "/" + totalAns);
     }
+
     public void showAns() {
         TextView displayInteger = (TextView) findViewById(
                 R.id.answer);
-        displayInteger.setText("Correct answer: "+ currentCat.getCatName() );
+        displayInteger.setText("Correct answer: " + currentCat.getCatName());
     }
 }
