655541b4e21c0923309678c1cb2c00a9f30e96c0
Stian Kvamme <stian.kvamme98@gmail.com>
2021-02-14 13:44:03 +0100

    added test for navigation

--- app/src/androidTest/java/com/example/navdrawerdemo/EspressoTest.java
+++ app/src/androidTest/java/com/example/navdrawerdemo/EspressoTest.java
@@ -1,10 +1,7 @@
 package com.example.navdrawerdemo;
 
 import android.content.Context;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.action.ViewActions;
@@ -12,7 +9,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import org.junit.After;
-import org.junit.Assert;
+;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -22,23 +19,14 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
-import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
-import static androidx.test.espresso.Espresso.onData;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.hasSibling;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withTagValue;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
@@ -72,27 +60,16 @@ public class EspressoTest{
     }
 
 
-    /*
-    // public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
-    @Test
-    public void changeActivityOnClick() {
-        //onView(hasSibling(withId(R.id.to_quiz))).perform(click());
-        onData(anything())
-                .inAdapterView(hasSibling(withId(R.id.to_quiz))).perform(click());
-        // This view is in a different Activity, no need to tell Espresso.
-        onView(hasSibling(withId(R.id.score))).check(matches(isDisplayed()));
-    }
 
     @Test
-    public void scoreIsCorrect() {
-        onView(withId(R.id.button1));
+    public void navigationTest() {
+        onView(withId(R.id.toolbarBurger)).perform(click());
+        onView(withId(R.id.addToQuiz)).perform(click());
     }
-    */
 
 
     @Test
     public void updateScoreOnCorrectAns(){ // insert value edit check if score update
-        List<CatObject> catList=catDao.getAll();
         MainActivity.getCurrentCat();
         String fasit =   MainActivity.getCurrentCat().getCatName();
         onView(withId(R.id.editText1)).perform((typeText(fasit)), ViewActions.closeSoftKeyboard());
